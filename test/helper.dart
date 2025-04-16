665bee2997f2eaadad75630b4cb796ed3b76c98b
Isaque Neves <isaque.neves@riodasostras.rj.gov.br>
2025-04-15 21:46:37 -0300

    implementado blueprint e migrations ainda precisa de testes

--- test/helper.dart
+++ test/helper.dart
@@ -11,7 +11,7 @@ class FakeConnection implements ConnectionInterface {
 
   @override
   QueryExpression raw(value) {
-    throw UnimplementedError();
+    return QueryExpression(value);
   }
 
   @override
